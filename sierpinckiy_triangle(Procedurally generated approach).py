0c217af45a823dcca03d1dc21284104ecc0863a6
Deff <flus7107@gmail.com>
2026-07-26 01:49:40 +0300

    cleaned up

--- sierpinckiy_triangle(Procedurally generated approach).py
+++ sierpinckiy_triangle(Procedurally generated approach).py
@@ -5,25 +5,16 @@ rows = {}
 
 for i in range(0, size):
     rows[i] = []
-
     if i > 2: del rows[i-2]
 
     triangle_view += (size - i) * " "
     
     for j in range(i):
-        value = None
         triangle_view += " "
-
-        if i < 2 or i == size - 1 or j == 0 or j == i - 1: 
-            rows[i].append(1) 
-            triangle_view += draw_symbol
-            
-        elif i > 1: 
-            value = rows[i-1][j-1] + rows[i-1][j]
-            rows[i].append(rows[i-1][j-1] + rows[i-1][j])
-
-        if value != None: triangle_view += draw_symbol if value % 2 == 1 else " "
-        
+        if i < 2 or i == size - 1 or j == 0 or j == i - 1: val = 1
+        elif i > 1: val = rows[i-1][j-1] + rows[i-1][j] 
+        triangle_view += draw_symbol if val % 2 == 1 else " "
+        rows[i].append(val)
     triangle_view += "\n"
 
 print(triangle_view)
